6d915f8ecdffb924e5d6944616e95029148b1d0c
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-02-13 18:07:27 -0500

    app.js main

--- rest_api-mongo-v1/app.js
+++ rest_api-mongo-v1/app.js
@@ -1,50 +1,100 @@
 'use strict';
+/*set up middleware with ... app.use( (req, res, next) => {});  */
 
 // load modules
 const express = require('express');
-const morgan = require('morgan');
-
-// variable to enable global error logging
-const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
-
-// create the Express app
 const app = express();
+const morgan = require('morgan'); //logger
+const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
+const bodyParser = require('body-parser').json; // parse incoming body of json data
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema; //db communication, add schema
+const Course = require("./models").Course;
+const User = require("./models").User;
+const routes = require("./routes");
+const auth = require("basic-auth");
+const bcrypt = require("bcryptjs");
+/*const jsonCheck = function(req, res, next) {
+  if(req.body){
+    console.log('what bag? ', req.body.color);
+  }
+  else{
+    console.log('no body props on request bc json isnt here yet');
+  }
+  next();}; // did json come through? */
 
-// setup morgan which gives us http request logging
-app.use(morgan('dev'));
+/*MIDDLEWARE*/
+app.use(morgan('dev')); //status codes
+app.use(bodyParser()); //function to use returned middle-ware for json body parser
+//app.use(jsonCheck);
 
-// TODO setup your api routes here
+/*Mongoose config*/
+mongoose.connect('mongodb://localhost:27017/fsjstd-restapi', {useNewUrlParser: true});
 
-// setup a friendly greeting for the root route
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Welcome to the REST API project!',
-  });
+const db = mongoose.connection;
+
+db.on('error', function(err) {
+  console.error('connection error:', err);
 });
 
-// send 404 if no other route matched
+db.once("open", function() {
+  console.log("DB connection successful!");
+});
+
+// authorize user
+app.use((req, res, next) => {
+  const user = auth(req);
+  if(user) {
+    User.findOne({emailAddress: user.name}).then(data => {
+      if(data) {
+        return data;
+      } else {
+        res.status(401);
+        return next(new Error("Authorization Failed"));
+      }
+    }).then(data => {
+      bcrypt.compare(user.pass, data.password, (err, valid) => {
+        if(valid) {
+          req.auth = true;
+          req.userId = data._id;
+          req.user = user.name;
+          next();
+        } else {
+          res.status(401);
+          req.auth = false;
+          return next(new Error("Enter correct password"));
+        }
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+// lets bring in the routes
+app.use('/', routes);
+//app.use("/api/users", routes);
+/*something missing*/
+
+/*ERRORS*/
 app.use((req, res) => {
   res.status(404).json({
-    message: 'Route Not Found',
-  });
-});
+    message: 'Sorry, that route was not found',
+  })
+}); //  404
 
-// setup a global error handler
 app.use((err, req, res, next) => {
   if (enableGlobalErrorLogging) {
     console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
   }
-
   res.status(err.status || 500).json({
-    message: err.message,
-    error: {},
-  });
-});
+    message: err.message
+  })
+}); // global error handler
 
-// set our port
-app.set('port', process.env.PORT || 5000);
+/* PORT*/
+const port = process.env.PORT || 5000;
 
-// start listening on our port
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express server is listening on port ${server.address().port}`);
+app.listen(port, function(){
+	console.log("Express server is listening on port", port);
 });
